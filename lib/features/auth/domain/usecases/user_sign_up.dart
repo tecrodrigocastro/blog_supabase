959528628802f1e196c613136ece0eb08a3fbc73
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2024-04-07 11:25:56 -0300

    UserSignUpCase

--- lib/features/auth/domain/usecases/user_sign_up.dart
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -1,1 +1,30 @@
-class UserSignUpCase {}
+import 'package:blog_supabase/core/error/failure.dart';
+import 'package:blog_supabase/core/usecases/usecase.dart';
+import 'package:blog_supabase/features/auth/domain/repositories/auth_repository.dart';
+import 'package:fpdart/src/either.dart';
+
+class UserSignUpCase implements UseCase<String, UserSignUpCaseParams> {
+  final AuthRepository repository;
+
+  UserSignUpCase(this.repository);
+  @override
+  Future<Either<Failure, String>> call(UserSignUpCaseParams params) async {
+    return await repository.signUpWithEmailAndPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpCaseParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpCaseParams({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
